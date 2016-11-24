ce4f08f2d46e3edf1207355e7073703bfb59d5f1
rendaniluk <lukhwa11582924@gmail.com>
2016-11-24 09:42:32 +0200

    Example of object

---
+++ person.js
@@ -0,0 +1,9 @@
+var person = {
+  firstName : 'Rendani',
+  lastName : 'Lukhwa',
+  age : 27,
+  employed : false,
+  qualifications : ['matric','BSc Physics','Codeacademy certificate'],
+};
+
+console.log(person.firstName,person.qualifications);
